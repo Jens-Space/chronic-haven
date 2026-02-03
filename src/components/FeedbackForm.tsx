404952404c726db70378d19d695a8041021c6ed6
Jen <jen@example.com>
2026-02-03 11:08:52 +0000

    Update feedback email to jensspacefeedback@gmail.com

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -26,7 +26,7 @@ export default function FeedbackForm() {
 
     try {
       // Using FormSubmit.co - emails will be sent to the email in the form
-      const response = await fetch('https://formsubmit.co/ajax/jensspace2026@gmail.com', {
+      const response = await fetch('https://formsubmit.co/ajax/jensspacefeedback@gmail.com', {
         method: 'POST',
         headers: { 
           'Content-Type': 'application/json',
